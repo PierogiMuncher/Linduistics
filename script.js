2f68cfacde020047480b3f01ae9e9f32337c981e
PierogiMuncher <46285928+PierogiMuncher@users.noreply.github.com>
2024-04-20 09:43:21 -0400

    Day 2
    Day 1 was dunkin donuts, hint: the mecca of coffee

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const sentences = [
-"Dunkin donuts."
+"Ninety nine bottles of beer on the wall."
 ];
 const symbols = Array.from({length: 26}, (_, i) => `symbols/${i + 1}.svg`);
 let currentSentence = '';
